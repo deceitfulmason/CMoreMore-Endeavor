63f99edb385fbc078aa450fcb120fe075f2f89a5
Emerson Gabriel <deceitfulmason1987@gmail.com>
2026-02-06 21:55:04 -0300

    Update lista1_emerson_gabriel_2.cpp

--- code/lista1_emerson_gabriel/lista1_emerson_gabriel_2.cpp
+++ code/lista1_emerson_gabriel/lista1_emerson_gabriel_2.cpp
@@ -2,5 +2,13 @@
 
 int main(){
 
+int i;
 
+	std::cout << "Esses são todos os valores entre 1 e 100:";
+	for(i = 1; i <= 100; ++i) std::cout << " " << i;
+
+	std::cout << std::endl << "Esses são todos os valores entre 100 e 1:";
+	for (i = 100; i >= 1; --i) std::cout << " " << i;
+
+return 0;
 }
