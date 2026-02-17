d1638056e14cdfaec53d529559ae1cf456c2c48a
mason <deceitfulmason1987@gmail.com>
2026-02-17 13:48:06 -0300

    learning about classes

---
+++ code/Unidade 1/Listas/lista5_emerson_gabriel/learningclasses.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+using namespace std;
+
+//Classes and objects are two main aspects of object oriented progamming
+
+/*A class defines what an object should look like, and an object is
+created based on that class.
+
+Classes are archetypes, and objects are just things made based themselves
+on the classes.
+Ex.: Fruit is a class, Apples, bananas and mangos are objects.
+
+*/
+
+
+//When you create an object from a class, it inherits all the variables
+//and functions defined inside that class.
+
+//Class is a user-defined data type that we can use in our program, and it
+//works as an object cconstructorwdd
+
+
+//BASIC SYNTAX OF A CLASS
+//class "NameOfClass"
+class myClass{
+	//Declaring access specifier
+	public:
+	//Declaring attributes
+		int myNum;
+		string myString;
+};
+
+
+
+
+int main(){
+
+
+myClass myObj;
+	myObj.myNum = 15;
+	myObj.myString = "Hello";
+
+
+cout << myObj.myNum << endl << myObj.myString;
+
+
+return 0;
+}
