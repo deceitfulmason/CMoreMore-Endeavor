84c329c47280d7267799e7896fa1b85ad29ac65b
Mason <deceitfulmason1987@gmail.com>
2026-02-18 15:24:58 -0300

    Update lista5_emerson_gabriel.cpp

--- code/Unidade 1/Listas/lista5_emerson_gabriel/lista5_emerson_gabriel.cpp
+++ code/Unidade 1/Listas/lista5_emerson_gabriel/lista5_emerson_gabriel.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <cctype>
 using namespace std;
 
 class cidadaos
@@ -53,10 +54,32 @@ public:
         if (altura == 0) return 0;
         return peso / (altura * altura);
     }
+
+    string getCpfFormatado() const
+    {
+        if (cpf.length() != 11) return cpf; // CPF invalido
+        return cpf.substr(0,3) + "." + cpf.substr(3,3) + "." + cpf.substr(6,3) + "-" + cpf.substr(9,2);
+    }
 };
 
+// Protótipos das funções
+void mostrarMenu();
+void opcao1(cidadaos pessoas[], int &cadastros);
+void opcao2(cidadaos pessoas[], int cadastros);
+void opcao3(cidadaos pessoas[], int cadastros);
+void opcao4(cidadaos pessoas[], int cadastros);
+void opcaoInvalida();
+void opcao0();
+void retornarMenu();
 
-void mostrarMenu(), opcao1(cidadaos pessoas[], int &cadastros), opcao2(cidadaos pessoas[], int cadastros), opcao3(cidadaos pessoas[], int cadastros), opcao4(cidadaos pessoas[], int cadastros), opcaoInvalida(), opcao0(), retornarMenu();
+// Função auxiliar para validar CPF
+bool validarCPF(const string& cpf)
+{
+    if(cpf.length() != 11) return false;
+    for(char c : cpf)
+        if(!isdigit(c)) return false;
+    return true;
+}
 
 int main()
 {
@@ -80,11 +103,9 @@ int main()
         case 3:
             opcao3(pessoas, cadastros);
             break;
-
         case 4:
             opcao4(pessoas, cadastros);
             break;
-
         case 0:
             opcao0();
             break;
@@ -98,6 +119,8 @@ int main()
     return 0;
 }
 
+// =================== Funções ===================
+
 void opcao1(cidadaos pessoas[], int &cadastros)
 {
     int quantidade = 0;
@@ -108,30 +131,37 @@ void opcao1(cidadaos pessoas[], int &cadastros)
         cin >> quantidade;
 
         if (quantidade < 0 || quantidade > 10 - cadastros)
-            cout << "Quantidade de pessoas invalidas!" << endl;
+            cout << "Quantidade de pessoas invalida!" << endl;
 
     } while (quantidade < 0 || quantidade > 10 - cadastros);
 
     for (int i = 0; i < quantidade; i++)
     {
-        cin.ignore(); // limpar ENTER antes do getline
+        cin.ignore(); 
 
         string nome;
         cout << "Digite o nome completo da pessoa " << cadastros + 1 << ": ";
         getline(cin, nome);
         pessoas[cadastros].setarNome(nome);
 
+        string documento;
+        do
+        {
+            cout << "Digite o CPF da pessoa " << cadastros + 1 << " (apenas numeros, 11 digitos): ";
+            getline(cin, documento);
+            if (!validarCPF(documento))
+                cout << "CPF invalido! Tente novamente." << endl;
+        } while (!validarCPF(documento));
+        pessoas[cadastros].setarCpf(documento);
+
         int age;
         do
         {
             cout << "Digite a idade da pessoa " << cadastros + 1 << ": ";
             cin >> age;
-
             if (age <= 0 || age >= 123)
                 cout << "Idade invalida!" << endl;
-
         } while (age <= 0 || age >= 123);
-
         pessoas[cadastros].setarIdade(age);
 
         int estado;
@@ -141,12 +171,9 @@ void opcao1(cidadaos pessoas[], int &cadastros)
                  << cadastros + 1
                  << " (0 solteiro, 1 casado): ";
             cin >> estado;
-
             if (estado != 0 && estado != 1)
                 cout << "Valor invalido!" << endl;
-
         } while (estado != 0 && estado != 1);
-
         pessoas[cadastros].setarEstado((bool)estado);
 
         double height;
@@ -156,12 +183,9 @@ void opcao1(cidadaos pessoas[], int &cadastros)
                  << cadastros + 1
                  << " (em metros): ";
             cin >> height;
-
             if (height <= 0.4 || height >= 2.72)
                 cout << "Valor invalido!" << endl;
-
         } while (height <= 0.4 || height >= 2.72);
-
         pessoas[cadastros].setarAltura(height);
 
         double weight;
@@ -170,12 +194,9 @@ void opcao1(cidadaos pessoas[], int &cadastros)
             cout << "Digite o peso da pessoa "
                  << cadastros + 1 << ": ";
             cin >> weight;
-
             if (weight < 2.7 || weight > 635)
                 cout << "Valor invalido!" << endl;
-
         } while (weight < 2.7 || weight > 635);
-
         pessoas[cadastros].setarPeso(weight);
 
         double income;
@@ -184,12 +205,9 @@ void opcao1(cidadaos pessoas[], int &cadastros)
             cout << "Digite a renda da pessoa "
                  << cadastros + 1 << ": ";
             cin >> income;
-
             if (income <= 0)
                 cout << "Digite um valor maior que 0!" << endl;
-
         } while (income <= 0);
-
         pessoas[cadastros].setarRenda(income);
 
         cadastros++;
@@ -201,20 +219,18 @@ void opcao1(cidadaos pessoas[], int &cadastros)
 void opcao2(cidadaos pessoas[], int cadastros)
 {
     int escolha;
-
     do
     {
-        cout << endl;
-        cout << "===== VISUALIZAR DADOS =====" << endl;
+        cout << endl << "===== VISUALIZAR DADOS =====" << endl;
         cout << "(1) Nome" << endl;
-        cout << "(2) Idade" << endl;
-        cout << "(3) Estado civil" << endl;
-        cout << "(4) Altura" << endl;
-        cout << "(5) Peso" << endl;
-        cout << "(6) Renda" << endl;
+        cout << "(2) CPF" << endl;
+        cout << "(3) Idade" << endl;
+        cout << "(4) Estado civil" << endl;
+        cout << "(5) Altura" << endl;
+        cout << "(6) Peso" << endl;
+        cout << "(7) Renda" << endl;
         cout << "(0) Voltar ao menu principal" << endl;
         cout << "Escolha uma opcao: ";
-
         cin >> escolha;
 
         if (cadastros == 0 && escolha != 0)
@@ -226,54 +242,38 @@ void opcao2(cidadaos pessoas[], int cadastros)
         switch (escolha)
         {
         case 1:
-            cout << endl << "Mostrar nomes" << endl;
             for (int i = 0; i < cadastros; i++)
                 cout << i + 1 << " - " << pessoas[i].getNome() << endl;
             break;
-
         case 2:
-            cout << endl << "Mostrar idades" << endl;
+            for (int i = 0; i < cadastros; i++)
+                cout << i + 1 << " - " << pessoas[i].getCpfFormatado() << endl;
+            break;
+        case 3:
             for (int i = 0; i < cadastros; i++)
                 cout << i + 1 << " - " << pessoas[i].getIdade() << endl;
             break;
-
-        case 3:
-            cout << endl << "Mostrar estados civis" << endl;
-            for (int i = 0; i < cadastros; i++)
-            {
-                cout << i + 1 << " - ";
-                if (pessoas[i].getCasado())
-                    cout << "Casado";
-                else
-                    cout << "Solteiro";
-                cout << endl;
-            }
-            break;
-
         case 4:
-            cout << endl << "Mostrar alturas" << endl;
+            for (int i = 0; i < cadastros; i++)
+                cout << i + 1 << " - " << (pessoas[i].getCasado() ? "Casado" : "Solteiro") << endl;
+            break;
+        case 5:
             for (int i = 0; i < cadastros; i++)
                 cout << i + 1 << " - " << pessoas[i].getHeight() << endl;
             break;
-
-        case 5:
-            cout << endl << "Mostrar pesos" << endl;
+        case 6:
             for (int i = 0; i < cadastros; i++)
                 cout << i + 1 << " - " << pessoas[i].getWeight() << endl;
             break;
-
-        case 6:
-            cout << endl << "Mostrar rendas" << endl;
+        case 7:
             for (int i = 0; i < cadastros; i++)
                 cout << i + 1 << " - " << pessoas[i].getIncome() << endl;
             break;
-
         case 0:
-            cout << endl << "Retornando ao menu principal..." << endl;
+            cout << "Retornando ao menu principal..." << endl;
             break;
-
         default:
-            cout << endl << "Opcao invalida!" << endl;
+            cout << "Opcao invalida!" << endl;
             break;
         }
 
@@ -281,6 +281,7 @@ void opcao2(cidadaos pessoas[], int cadastros)
 
     retornarMenu();
 }
+
 
 
 void opcao3(cidadaos pessoas[], int cadastros)
@@ -293,7 +294,6 @@ void opcao3(cidadaos pessoas[], int cadastros)
     }
 
     double somaIdade = 0, somaRenda = 0, somaAltura = 0, somaPeso = 0;
-
     for (int i = 0; i < cadastros; i++)
     {
         somaIdade += pessoas[i].getIdade();
@@ -308,7 +308,6 @@ void opcao3(cidadaos pessoas[], int cadastros)
     double mediaPeso = somaPeso / cadastros;
 
     double dpIdade = 0, dpRenda = 0, dpAltura = 0, dpPeso = 0;
-
     for (int i = 0; i < cadastros; i++)
     {
         dpIdade += pow(pessoas[i].getIdade() - mediaIdade, 2);
@@ -337,7 +336,6 @@ void opcao3(cidadaos pessoas[], int cadastros)
     retornarMenu();
 }
 
-
 void opcao4(cidadaos pessoas[], int cadastros)
 {
     if (cadastros == 0)
@@ -352,21 +350,12 @@ void opcao4(cidadaos pessoas[], int cadastros)
     for (int i = 0; i < cadastros; i++)
     {
         double imc = pessoas[i].calcularIMC();
-
-        cout << i + 1 << " - " << pessoas[i].getNome()
-             << " | IMC: " << imc;
-
-        if (imc >= 30)
-            cout << " | Obeso";
-        else
-            cout << " | Nao obeso";
-
-        cout << endl;
+        cout << i + 1 << " - " << pessoas[i].getNome() << " | IMC: " << imc;
+        cout << (imc >= 30 ? " | Obeso" : " | Nao obeso") << endl;
     }
 
     retornarMenu();
 }
-
 
 void opcao0()
 {
